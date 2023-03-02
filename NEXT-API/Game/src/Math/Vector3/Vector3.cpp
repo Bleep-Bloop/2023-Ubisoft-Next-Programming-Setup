708cbcfac42c15d1c08298c65b3f0280f4d991e6
Taisen Colcher <TaisenColcher@Gmail.com>
2023-03-02 15:11:31 -0500

    Add Vector3 operator implementation
    Added implementation for operators:
    +, +=, -, -=, *, *=, /, /=, ==, !=

--- NEXT-API/Game/src/Math/Vector3/Vector3.cpp
+++ NEXT-API/Game/src/Math/Vector3/Vector3.cpp
@@ -1,2 +1,102 @@
 #include "stdafx.h"
 #include "Vector3.h"
+
+Vector3::Vector3() : x(0.0f), y(0.0f), z(0.0f)
+{
+}
+
+Vector3::Vector3(float inX, float inY, float inZ) : x(inX), y(inY), z(inZ)
+{
+}
+
+Vector3 Vector3::operator+(const Vector3& other) const
+{
+	return Vector3(this->x + other.x, this->y + other.y, this->z + other.z);
+}
+
+Vector3& Vector3::operator+=(const Vector3& other)
+{
+	x += other.x;
+	y += other.y;
+	z += other.z;
+	return *this;
+}
+
+Vector3 Vector3::operator-(const Vector3& other) const
+{
+	return Vector3(this->x - other.x, this->y - other.y, this->z - other.z);
+}
+
+Vector3& Vector3::operator-=(const Vector3& other)
+{
+	x -= other.x;
+	y -= other.y;
+	z -= other.z;
+	return *this;
+}
+
+Vector3 Vector3::operator*(float scalar) const
+{
+	return Vector3(this->x * scalar, this->y * scalar, this->z * scalar);
+}
+
+Vector3 Vector3::operator*=(float scalar)
+{
+	x *= scalar;
+	y *= scalar;
+	z *= scalar;
+	return *this;
+}
+
+Vector3 Vector3::operator/(float scalar) const
+{
+	return Vector3(this->x / scalar, this->y / scalar, this->z / scalar);
+}
+
+Vector3 Vector3::operator/=(float scalar)
+{
+	x /= scalar;
+	y /= scalar;
+	z /= scalar;
+	return *this;
+}
+
+bool Vector3::operator==(const Vector3& other) const
+{
+	return x == other.x && y == other.y && z == other.z;
+}
+
+bool Vector3::operator!=(const Vector3& other) const // ***
+{
+	return x != other.x || y != other.y || z != other.z;
+}
+
+Vector3 Vector3::Normalize() const
+{
+	return Vector3();
+}
+
+float Vector3::Magnitude() const
+{
+	return 0.0f;
+}
+
+float Vector3::SqrMagnitude() const
+{
+	return 0.0f;
+}
+
+float Vector3::Dot(const Vector3& other) const
+{
+	return 0.0f;
+}
+
+Vector3 Vector3::Cross(const Vector3& other) const
+{
+	return Vector3();
+}
+
+std::string Vector3::ToString()
+{
+	return std::string();
+}
