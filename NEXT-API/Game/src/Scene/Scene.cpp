c93c6bc209fc5ff806072dce2729b1fff3298b04
Taisen Colcher <TaisenColcher@Gmail.com>
2023-03-03 12:12:40 -0500

    Add Time handling
    Added <chrono> as it is more reliable then glutGet(GLUT_ELAPSED_TIME).
    Added member variables m_deltaTime, m_start, m_current, m_time
    Added Update(), GeteDeltaTime(), and GetTime().
    Added Init() with ToDo: comments for unplanned classes.

--- NEXT-API/Game/src/Scene/Scene.cpp
+++ NEXT-API/Game/src/Scene/Scene.cpp
@@ -1,2 +1,25 @@
 #include "stdafx.h"
 #include "Scene.h"
+
+Scene::Scene()
+{
+}
+
+void Scene::Init()
+{
+}
+
+void Scene::Update(float deltaTime)
+{
+}
+
+float Scene::GetDeltaTime()
+{
+	return m_deltaTime;
+}
+
+float Scene::GetTime()
+{
+	return m_time.count();
+}
+
--- NEXT-API/Game/src/Scene/Scene.h
+++ NEXT-API/Game/src/Scene/Scene.h
@@ -1,6 +1,28 @@
 #pragma once
 
+#include <chrono> // more reliable then glutGet(GLUT_ELAPSED_TIME); ToDo: Note in documentation
+class Vector3;
+
 class Scene
 {
+public: 
+	Scene();
+
+	void Init(); // ToDo: Finish initializer &Renderer and &UI
+
+	void Update(float deltaTime);
+
+	float GetDeltaTime();
+
+	float GetTime();
+private:
+	// Time
+	float m_deltaTime; // time since last update
+	std::chrono::time_point<std::chrono::steady_clock> m_start;
+	std::chrono::time_point<std::chrono::steady_clock> m_current;
+	std::chrono::duration<float> m_time; // time passed since beginning of cycle
+	
+
+	// ToDo: Pause(), GetUI()
 };
 
