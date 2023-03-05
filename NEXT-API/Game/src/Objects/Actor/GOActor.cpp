18720972e1710329e6f83bc02e62b14476d98b64
Taisen Colcher <TaisenColcher@Gmail.com>
2023-03-05 06:18:41 -0500

    Update GOActor
    .h
    Added function to HandlePlayerInput().
    Added Render() override to handle rendering.
    Added float deltaTime.
    Added SetDeltaTime();
    Added Destroy() override;
    
    .cpp
    Added include scene.
    Added Move() implementation.
    Added HandlePlayerInput() implementation
    Render() implementation.
    SetDeltaTime() implementation.
    Destroy() implementation.

--- NEXT-API/Game/src/Objects/Actor/GOActor.cpp
+++ NEXT-API/Game/src/Objects/Actor/GOActor.cpp
@@ -1,5 +1,6 @@
 #include "stdafx.h"
 #include "GOActor.h"
+#include "../src/Scene/Scene.h"
 
 // ToDo: Come back to constructor
 GOActor::GOActor(Scene& scene, CSimpleSprite& sprite, Transform transform)
@@ -9,12 +10,13 @@ GOActor::GOActor(Scene& scene, CSimpleSprite& sprite, Transform transform)
 	this->SetTransform(transform);
 
 	this->scene = &scene;
+	
 }
 
 void GOActor::Move(MovementDirection direction)
 {
-	// Note: Make sure to set posistion when creating sprite to set the values properly.
-	// this->sprite->Update(deltaTime); // Important! Call here after putting Move in Tick()
+
+	this->sprite->Update(deltaTime);
 	switch (direction)
 	{
 	default:
@@ -32,22 +34,66 @@ void GOActor::Move(MovementDirection direction)
 		y -= characterMoveSpeed;
 		this->sprite->SetPosition(x, y);
 		break;
+	case MovementDirection::RIGHT:
+		//this->sprite->SetAnimation(ANIM_RIGHT);
+		this->sprite->GetPosition(x, y);
+		x += characterMoveSpeed;
+		this->sprite->SetPosition(x, y);
+		break;
 	case MovementDirection::LEFT:
 		//this->sprite->SetAnimation(ANIM_LEFT);
 		this->sprite->GetPosition(x, y);
 		x -= characterMoveSpeed;
 		this->sprite->SetPosition(x, y);
 		break;
-	case MovementDirection::RIGHT:
-		//this->sprite->SetAnimation(ANIM_RIGHT);
-		float x, y;
-		this->sprite->GetPosition(x, y);
-		x += characterMoveSpeed;
+
 	}
 }
 
 void GOActor::SetCharacterMoveSpeed(float MoveSpeed)
 {
 	this->characterMoveSpeed = MoveSpeed;
+}
+
+void GOActor::HandlePlayerInput()
+{
+	// ToDo: Two Controllers? Implementation is there but then requires 2 reviewers to play. 
+	// ToDo: Allow for both? GameManager does not care how characters are controlled they 
+	// are seperate entitiies.
+	
+	if (App::GetController().GetLeftThumbStickY() > 0.5f)
+	{
+		Move(MovementDirection::DOWN);
+	}
+	if (App::GetController().GetLeftThumbStickY() < -0.5f)
+	{
+		Move(MovementDirection::UP);
+	}
+	if (App::GetController().GetLeftThumbStickX() > 0.5f)
+	{
+		Move(MovementDirection::RIGHT);
+	}
+	if (App::GetController().GetLeftThumbStickX() < -0.5f)
+	{
+		Move(MovementDirection::LEFT);
+	}
+	
+	// ToDo - Bomb Drop
+
+}
+
+void GOActor::Render()
+{
+	GameObject::Render();
+}
+
+void GOActor::SetDeltaTime(float deltaTime)
+{
+	this->deltaTime = deltaTime;
+}
+
+void GOActor::Destroy()
+{
+	delete this;
 }
 
