4184afa4d88557ec67af17a75c9cb48e07ee9b77
Taisen Colcher <TaisenColcher@Gmail.com>
2023-03-05 06:11:07 -0500

    Update Scene.h
    Added vector for GOActors m_actors.
    Added AddActor() function to add to vector.
    Reworded comments.
    Added ToDo comments.

--- NEXT-API/Game/src/Scene/Scene.cpp
+++ NEXT-API/Game/src/Scene/Scene.cpp
@@ -1,7 +1,8 @@
 #include "stdafx.h"
 #include "Scene.h"
 
-Scene::Scene()
+
+Scene::Scene() : m_deltaTime(0.0f), m_id(0) 
 {
 
 	// Initialize Time Variables
@@ -9,19 +10,49 @@ Scene::Scene()
 	m_current = m_start;
 	m_time = m_current - m_start;
 
-
+	
 }
 
 void Scene::Init()
 {
+
+	// ToDo: If out of time generate level here 
+
 }
-
-
 
 void Scene::Update(float deltaTime)
 {
 	SetTime(deltaTime);
+
+	for (auto& id : m_actors)
+	{
+		// where arrow go
+		id.HandlePlayerInput();
+	}
+
+	// On creation I should add
+	//for (auto &id : m_actors)
+	//{
+		// this way looks ok
+		// id->HandleInput
+	//}
+
 }
+
+
+void Scene::SetTime(float deltaTime)
+{
+	m_deltaTime = deltaTime;
+	m_current = std::chrono::steady_clock::now();
+	m_time = m_current - m_start;
+}
+
+
+void Scene::AddActor(GOActor &actor)
+{
+	m_actors.push_back(actor);
+}
+
 
 float Scene::GetDeltaTime()
 {
@@ -53,9 +84,3 @@ void Scene::SetCollider(int id, Collider collider)
 	m_collider[id] = collider;
 }
 
-void Scene::SetTime(float deltaTime)
-{
-	m_deltaTime = deltaTime;
-	m_current = std::chrono::steady_clock::now();
-	m_time = m_current - m_start;
-}
