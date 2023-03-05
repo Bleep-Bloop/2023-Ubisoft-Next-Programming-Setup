d971a4ec0181233dd462a6bf11e9a66f475a83fa
Taisen Colcher <TaisenColcher@Gmail.com>
2023-03-04 22:07:34 -0500

    Add GOActor class
    Created folder 'src'->'Objects'->'Actor'
    Added GOActor class there.
    Added comment explaining class.

---
+++ NEXT-API/Game/src/Objects/Actor/GOActor.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "../GameObject.h"
+
+/// <summary>
+/// GOActor is a GameObject that has built-in movement logic. They are responsible for physical interaction between 
+/// Player/AI and the game world.
+/// </summary>
+class GoActor : GameObject
+{
+
+
+
+};
+
