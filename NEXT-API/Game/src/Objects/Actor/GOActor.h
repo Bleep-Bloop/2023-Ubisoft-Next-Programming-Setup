68641859559467f98a608156db8d33d87554cc95
Taisen Colcher <TaisenColcher@Gmail.com>
2023-03-04 23:40:36 -0500

    Add GOActor class
    Created new folder 'src'->'Objects'->'Actor'
    Placed GOActor there.
    Added constructor.
    Added ToDo note about animations.

--- NEXT-API/Game/src/Objects/Actor/GOActor.h
+++ NEXT-API/Game/src/Objects/Actor/GOActor.h
@@ -1,6 +1,16 @@
 #pragma once
 
 #include "../GameObject.h"
+#include "../../App/SimpleSprite.h"
+
+/////struct sAnimation; // Handle this in the character class then just pass it to 
+/*{
+	unsigned int m_id;
+	float m_speed;
+	std::vector<int> m_frames;
+};
+*/
+
 
 /// <summary>
 /// GOActor is a GameObject that has built-in movement logic. They are responsible for physical interaction between 
@@ -9,6 +19,15 @@
 class GoActor : GameObject
 {
 
+	GoActor(Scene& scene, CSimpleSprite* sprite, Transform transform);
+
+
+
+	/// <summary>
+	/// Sets animation to be played
+	/// </summary>
+	// ToDo: void PlayAnimation();
+	// ToDo/Question: Should I create animations in a character class/struct that holds all the information for said character?
 
 
 };
