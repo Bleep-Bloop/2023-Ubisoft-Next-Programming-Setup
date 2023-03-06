820b4e20e23421388862414f47b99fe995578945
Taisen Colcher <TaisenColcher@Gmail.com>
2023-03-05 19:15:02 -0500

    Add Physics struct
    Added a physics struct.
    Added constructor.
    Added Vector3 velocity.
    Added Vector3 acceleration.

---
+++ NEXT-API/Game/src/Components/Physics/Physics.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "../../Math/Vector3/Vector3.h"
+
+struct Physics
+{
+
+	Physics();
+
+	Vector3 velocity;	// Represents the rate of change of the objects position. 
+	Vector3 acceleration; 
+
+};
+
